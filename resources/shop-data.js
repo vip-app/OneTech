e19e77b1448e6470a381ca981ab9acf9adb2507c
vip-app <seemenu.ru@gmail.com>
2023-04-15 07:00:50 +0300

    70015042023

---
+++ resources/shop-data.js
@@ -0,0 +1,9 @@
+const data = {
+		title: "OneTech",
+		tel: "+79068005357",
+		whatsapp: "",
+		telegram: "",
+		image: "",
+		email: "fastsales@gmail.com",
+		address: ""
+	};
---
+++ resources/test2.js
@@ -0,0 +1,164 @@
+const featured2 = [
+  {
+    id: 1,
+    title: "Ноутбук ASUS ZenBook",
+    img: "images/featured_1.png",
+    price: 49999,
+    discount: 15,
+    description: "Элегантный и мощный ноутбук с диагональю экрана 13.3 дюйма и процессором Intel Core i7.",
+    isNew: true
+  },
+  {
+    id: 2,
+    title: "Смартфон Apple iPhone 13",
+    img: "images/featured_2.png",
+    price: 89999,
+    discount: 0,
+    description: "Новый смартфон Apple с OLED-дисплеем, 128 ГБ памяти и технологией Face ID.",
+    isNew: true
+  },
+  {
+    id: 3,
+    title: "Телевизор Samsung QLED QN90A",
+    img: "images/featured_3.png",
+    price: 149999,
+    discount: 20,
+    description: "Качественный телевизор с диагональю 65 дюймов, поддержкой HDR и функцией Smart TV.",
+    isNew: false
+  },
+  {
+    id: 4,
+    title: "Планшет Lenovo Yoga Tab 13",
+    img: "images/featured_4.png",
+    price: 37999,
+    discount: 0,
+    description: "Планшет с диагональю экрана 13 дюймов, с выдвижной подставкой и звуковой системой JBL.",
+    isNew: true
+  },
+  {
+    id: 5,
+    title: "Ноутбук Acer Predator Helios 500",
+    img: "images/featured_5.png",
+    price: 129999,
+    discount: 0,
+    description: "Игровой ноутбук с процессором Intel Core i7, дискретной графикой NVIDIA GeForce GTX 1070 и 17.3-дюймовым экраном.",
+    isNew: false
+  },
+  {
+    id: 6,
+    title: "Ноутбук ASUS ZenBook",
+    img: "images/featured_6.png",
+    price: 49999,
+    discount: 15,
+    description: "Элегантный и мощный ноутбук с диагональю экрана 13.3 дюйма и процессором Intel Core i7.",
+    isNew: true
+  },
+  {
+    id: 7,
+    title: "Смартфон Apple iPhone 13",
+    img: "images/featured_7.png",
+    price: 89999,
+    discount: 10,
+    description: "Новый смартфон Apple с OLED-дисплеем, 128 ГБ памяти и технологией Face ID.",
+    isNew: true
+  },
+  {
+    id: 8,
+    title: "Телевизор Samsung QLED QN90A",
+    img: "images/featured_8.png",
+    price: 149999,
+    discount: 20,
+    description: "Качественный телевизор с диагональю 65 дюймов, поддержкой HDR и функцией Smart TV.",
+    isNew: false
+  },
+  {
+    id: 9,
+    title: "Планшет Lenovo Yoga Tab 13",
+    img: "images/featured_2.png",
+    price: 37999,
+    discount: 5,
+    description: "Планшет с диагональю экрана 13 дюймов, с выдвижной подставкой и звуковой системой JBL.",
+    isNew: true
+  },
+  {
+    id: 10,
+    title: "Ноутбук Acer Predator Helios 500",
+    img: "images/featured_3.png",
+    price: 129999,
+    discount: 0,
+    description: "Игровой ноутбук с процессором Intel Core i7, дискретной графикой NVIDIA GeForce GTX 1070 и 17.3-дюймовым экраном.",
+    isNew: false
+  },
+  {
+    id: 11,
+    title: "Наушники Sony WH-1000XM4",
+    img: "images/featured_4.png",
+    price: 29990,
+    discount: 10,
+    description: "Беспроводные наушники с шумоподавлением и технологией LDAC.",
+    isNew: false
+  },
+  {
+    id: 12,
+    title: "Ноутбук Apple MacBook Air",
+    img: "images/featured_5.png",
+    price: 99990,
+    discount: 0,
+    description: "Легкий и компактный ноутбук с диагональю экрана 13 дюймов и процессором Apple M1.",
+    isNew: true
+  },
+  {
+    id: 13,
+    title: "Смартфон Samsung Galaxy S21",
+    img: "images/featured_6.png",
+    price: 69990,
+    discount: 15,
+    description: "Смартфон с OLED-дисплеем, 128 ГБ памяти и камерой с тройным модулем.",
+    isNew: false
+  },
+  {
+    id: 14,
+    title: "Умные часы Apple Watch Series 7",
+    img: "images/featured_7.png",
+    price: 39990,
+    discount: 5,
+    description: "Умные часы с дисплеем 1.9 дюйма и защитой от падений и пыли.",
+    isNew: true
+  },
+  {
+    id: 15,
+    title: "Игровой компьютер Alienware Aurora R12",
+    img: "images/featured_8.png",
+    price: 189990,
+    discount: 20,
+    description: "Игровой компьютер с процессором Intel Core i9, видеокартой NVIDIA GeForce RTX 3080 и 32 ГБ оперативной памяти.",
+    isNew: false
+  },
+  {
+    id: 16,
+    title: "Ноутбук ASUS ZenBook",
+    img: "images/featured_1.png",
+    price: 49999,
+    discount: 15,
+    description: "Элегантный и мощный ноутбук с диагональю экрана 13.3 дюйма и процессором Intel Core i7.",
+    isNew: true
+  },
+  {
+    id: 17,
+    title: "Смартфон Apple iPhone 13",
+    img: "images/featured_3.png",
+    price: 89999,
+    discount: 10,
+    description: "Новый смартфон Apple с OLED-дисплеем, 128 ГБ памяти и технологией Face ID.",
+    isNew: true
+  },
+  {
+    id: 18,
+    title: "Телевизор Samsung QLED QN90A",
+    img: "images/featured_8.png",
+    price: 149999,
+    discount: 20,
+    description: "Качественный телевизор с диагональю 65 дюймов, поддержкой HDR и функцией Smart TV.",
+    isNew: false
+  }
+];
