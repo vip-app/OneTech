e92174e054e26d7a41b69ce9df718607aa91dc5b
vip-app <seemenu.ru@gmail.com>
2023-04-23 15:36:42 +0300

    153623042023

--- resources/shop-data.js
+++ resources/shop-data.js
@@ -1,9 +1,9 @@
 const data = {
 		title: "OneTech",
-		tel: "+79068005357",
-		whatsapp: "",
+		tel: "79640626525",
+		whatsapp: "79640626525",
 		telegram: "",
 		image: "",
 		email: "seemenu.ru@gmail.com",
-		address: ""
+		address: "43.6692488751,45.1036912278"
 	};
